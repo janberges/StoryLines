03a24fd6ee1d8036ab92b7c15528c43caab0add0
Jan Berges <jan.berges@uni-bremen.de>
2016-03-12 20:56:59 +0100

    Provide minimal application example

---
+++ example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import numpy as np
+import storylines
+
+plot = storylines.Plot()
+
+t = np.linspace(-1, 1, 100)
+
+for omega in np.linspace(0, np.pi, 10):
+    plot.line(t, np.sin(omega * t), omega)
+
+plot.xlabel = '$t / \mathrm s$'
+plot.ylabel = '$\sin(\omega t)$'
+plot.zlabel = '$\omega / \mathrm{s^{-1}}$'
+
+plot.save('example.sl')
