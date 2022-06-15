ff7b6de8267abb18d5488d24f5af12ab041090f7
Jan Berges <jan.berges@uni-bremen.de>
2022-06-15 09:26:33 +0200

    Create new initialization file

---
+++ storylines/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2016-2022 Jan Berges
+# This program is free software under the terms of the BSD Zero Clause License.
+
+from __future__ import division
+
+__version__ = '0.7'
+
+from .plot import Plot
+from .calc import (order_of_magnitude, power_of_ten, xround, xround_mantissa,
+    multiples, dot, cross, bonds)
+from .color import Color, colormap, colorize, HSV2RGB, RGB2HSV, PSV2RGB
+from .convert import inch, pt, csv
+from .cut import relevant, shortcut, cut, cut2d, jump
+from .fatband import fatband, miter_butt
+from .files import goto, typeset, rasterize, combine
+from .group import islands, groups
+from .png import save, load
+from .proj import projection, project
--- storylines/plot.py
+++ storylines/plot.py
@@ -3,23 +3,17 @@
 # Copyright (C) 2016-2022 Jan Berges
 # This program is free software under the terms of the BSD Zero Clause License.
 
-from __future__ import division
-
-__version__ = '0.7'
-
 import math
 import re
 
-from .calc import (order_of_magnitude, power_of_ten, xround, xround_mantissa,
-    multiples, dot, cross, bonds)
-from .color import Color, colormap, colorize, HSV2RGB, RGB2HSV, PSV2RGB
+from .calc import power_of_ten, xround_mantissa, multiples
+from .color import colorize
 from .convert import inch, pt, csv
-from .cut import relevant, shortcut, cut, cut2d, jump
+from .cut import relevant, shortcut, cut2d, jump
 from .fatband import fatband, miter_butt
-from .files import goto, typeset, rasterize, combine
+from .files import goto, typeset, rasterize
 from .group import islands, groups
-from .png import save, load
-from .proj import projection, project
+from .png import save
 
 class Plot():
     """Plot object.
