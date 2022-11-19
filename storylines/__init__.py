8322c75bdbde7c3a428c2f2b8500f9d020bf91a7
Jan Berges <jan.berges@uni-bremen.de>
2022-11-19 19:15:52 +0100

    Increment version number

--- storylines/__init__.py
+++ storylines/__init__.py
@@ -3,7 +3,7 @@
 
 """Line plots with Python & TikZ."""
 
-__version__ = '0.8'
+__version__ = '0.9'
 
 from .plot import Plot
 from .calc import (order_of_magnitude, power_of_ten, xround, xround_mantissa,
