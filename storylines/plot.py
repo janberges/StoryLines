a0d360c4b0d9134fea5afda630e3ee4c90b99eb1
Jan Berges <jan.berges@uni-bremen.de>
2026-04-21 16:56:22 +0200

    Do not skip repeated special legend entries

--- storylines/plot.py
+++ storylines/plot.py
@@ -1390,7 +1390,8 @@ class Plot():
 
                     for previous in labels:
                         if label[1] and previous == label:
-                            break
+                            if not re.search(r'^\*.*\*$', label[1]):
+                                break
                     else:
                         labels.append(label)
 
